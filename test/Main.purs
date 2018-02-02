153e7baa543f3e31be76cc19a68250b3fee9fe14
Matthew Leon <ml@matthewleon.com>
2018-02-02 17:35:19 -0500

    use unfoldr for test

--- test/Main.purs
+++ test/Main.purs
@@ -1,14 +1,15 @@
 module Test.Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Console.BrowserSpecific.Timer (time, timeEnd)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.List (length, reverse)
-import Data.List.Types (List(..), (:))
+import Data.List (List, length)
+import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
-
+import Data.Unfoldable (unfoldr)
 import System.Random.Xorshift32 (RNGState, seed, int32)
 
 main :: Eff (console :: CONSOLE, exception :: EXCEPTION) Unit
@@ -17,14 +18,11 @@ main = do
   let ints = randomInts 100 1000000
   timeEnd timer
   log $ "generated list of " <> show (length ints) <> " random ints."
-
   where
-    randomInts :: Int -> Int -> List Int
-    randomInts s length = reverse $ go (seed s) Nil 0
-      where
-        go :: RNGState -> List Int -> Int -> List Int
-        go state accum index -- tail recursive, builds backward
-          | (index < length) =
-            case int32 state
-              of (Tuple i state') -> go state' (i : accum) (index + 1)
-          | otherwise = accum
+  randomInts :: Int -> Int -> List Int
+  randomInts s = unfoldr go <<< Tuple (seed s)
+    where
+    go :: (Tuple RNGState Int) -> Maybe (Tuple Int (Tuple RNGState Int))
+    go (Tuple rngState n)
+      | n > 0     = Just $ (_ `Tuple` n - 1) <$> int32 rngState
+      | otherwise = Nothing
