8e78744b55d5aff990b87fe8f4e4a8409475d50a
Matthew Leon <ml@matthewleon.com>
2018-02-07 18:53:19 -0800

    eliminate some $ for optimization

--- src/System/Random/Xorshift32.purs
+++ src/System/Random/Xorshift32.purs
@@ -12,7 +12,7 @@ instance showSeed :: Show Seed where
 
 seed :: Int -> Maybe Seed
 seed 0 = Nothing
-seed i = Just $ Seed i
+seed i = Just (Seed i)
 
 getSeed :: Seed -> Int
 getSeed (Seed i) = i
@@ -21,4 +21,4 @@ int32 :: Seed -> Seed
 int32 (Seed x) =
   let x'   = x   .^. (x `shl` 13)
       x''  = x'  .^. (x' `shr` 17)
-   in Seed $ x'' .^. (x'' `shl` 5)
+   in Seed (x'' .^. (x'' `shl` 5))
